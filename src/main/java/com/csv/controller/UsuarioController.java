f3a0c7a3e99a992fdecee34cd77480ff522908a0
Adauto Bueno <adauto.bueno@gmail.com>
2022-10-29 23:39:27 -0300

    classe Usuario criada

---
+++ src/main/java/com/csv/controller/UsuarioController.java
@@ -0,0 +1,16 @@
+package com.csv.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/usuario")
+public class UsuarioController {
+	
+	@GetMapping("/index")
+	public String index() {
+		return "index";
+	} 
+
+}
