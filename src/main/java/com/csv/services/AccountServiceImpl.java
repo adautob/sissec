570f1a6f2483b7113eba469f57a5840b19e850a8
Adauto Bueno <adauto.bueno@gmail.com>
2022-11-01 23:27:05 -0300

    criado HomeController

--- src/main/java/com/csv/services/AccountServiceImpl.java
+++ src/main/java/com/csv/services/AccountServiceImpl.java
@@ -1,5 +1,36 @@
 package com.csv.services;
 
-public class AccountServiceImpl {
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import com.csv.model.Account;
+import com.csv.repositories.AccountRepository;
+
+public class AccountServiceImpl implements AccountService
+
+{
+	
+	@Autowired
+	private AccountRepository accountRepository;
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		Account account = accountRepository.find(username);
+		if (account == null) {
+			throw new UsernameNotFoundException("Username not found for "+username);
+		}
+		List<GrantedAuthority> grantedAuthorities = new ArrayList<GrantedAuthority>();
+		for (String role : account.getRoles()) {
+			grantedAuthorities.add(new SimpleGrantedAuthority(role));
+		}
+		return new User(account.getUsername(), account.getPassword(), grantedAuthorities);
+	}
 
 }
